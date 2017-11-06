9a2ebe97aca33188e3d6590eef6d867208989455
Jason Li <Yunf.li37@gmail.com>
2017-11-06 11:09:30 -0500

    basic layout

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,30 @@ connection.connect((error)=>{
 		console.log(error);
 	}
 });
-
-/* GET home page. */
+//================== HOME PAGE==================
 router.get('/', function(req, res, next) {
 	var selectQuery = "select * from voteUsers;";
 	connection.query(selectQuery, (error, results, field)=>{
 		res.render("index", {result:results});
 	});
+});
+// ==================REGISTER==================
+router.get("/register", (req,res,next)=>{
+	res.render("register");
+});
+
+router.post("/registerProcess", (req, res, next)=>{
+	res.json(req.body);
+});
+
+
+// ==================LOGIN==================
+router.get("/login", (req, res,next)=>{
+	res.render("login");
+});
+
+router.post("/loginProcess", (req, res, next)=>{
+	res.json(req.body);
 });
 
 module.exports = router;
